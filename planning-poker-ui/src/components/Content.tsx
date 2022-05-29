64e11fee151b8fc020ddc97de8d94fb26f8b9173
Ladislav Petera <ladislav.petera@yahoo.de>
2022-05-29 14:33:30 +0200

    base version for the workshop

--- planning-poker-ui/src/components/Content.tsx
+++ planning-poker-ui/src/components/Content.tsx
@@ -1,30 +1,8 @@
-import Room from './Room'
-import RoomSelection from './RoomSelection'
-import Welcome from './Welcome'
+import { Typography } from '@mui/material'
 
-interface Props {
-    name: string | undefined
-    room: number | undefined
-    setName: (name: string) => void
-    votes: Record<string, string>
-    joinRoom: (room: number) => void
-    createRoom: () => void
-    vote: (points: string) => void
-}
-
-const Content: React.FC<Props> = ({name, room, votes, setName, joinRoom, createRoom, vote}) => {
+const Content: React.FC = () => {
   return (
-    <>
-      {!name && <Welcome onSetName={setName} />}
-
-      {name && !room && (
-        <RoomSelection onEnterRoom={joinRoom} onCreateRoom={createRoom} />
-      )}
-
-      {name && room && (
-        <Room name={name} votes={votes} vote={vote}/>
-      )}
-    </>
+    <Typography>Content</Typography>
   )
 }
 
--- planning-poker-ui/src/components/Header.tsx
+++ planning-poker-ui/src/components/Header.tsx
@@ -1,88 +1,12 @@
-import {Cable, ExitToApp, Person, Refresh} from '@mui/icons-material'
-import {AppBar, IconButton, styled, Toolbar, Typography} from '@mui/material'
-import React, {Fragment} from 'react'
-import {ReadyState} from 'react-use-websocket'
+import { AppBar, Typography } from '@mui/material'
+import React from 'react'
 
-const Title = styled(Typography)`
-  display: block;
-  ${({theme}) => theme.breakpoints.down('sm')} {
-    display: none;
-  }
-`
-
-const Room = styled(Typography)`
-  flex-grow: 1;
-  ${({theme}) => theme.breakpoints.up('sm')} {
-    text-align: center;
-  }
-`
-
-interface Props {
-  room?: number
-  userName?: string
-  onNewRound: () => void
-  onLeaveRoom: () => void
-  onSetUserName: (name: string | undefined) => void
-  readyState: ReadyState
-}
-
-const Header: React.FC<Props> = ({
-  room,
-  userName,
-  onNewRound,
-  onLeaveRoom,
-  onSetUserName,
-  readyState
-}) => {
-  const StatusIndicator: React.FC = () => {
-    switch (readyState) {
-      case ReadyState.OPEN:
-        return <Cable color='success' />
-      case ReadyState.CONNECTING:
-        return <Cable color='inherit' />
-      default:
-        return <Cable color='error' />
-    }
-  }
-
+const Header: React.FC = () => {
   return (
     <AppBar position='static'>
-      <Toolbar>
-        <StatusIndicator />
-
-        <Title variant='h6' noWrap>
-          Planning Poker
-        </Title>
-
-        {room && (
-          <Fragment>
-            <Room variant='h6' noWrap>
-              Room: {room} ({userName})
-            </Room>
-          </Fragment>
-        )}
-
-        {room ? (
-          <Fragment>
-            <IconButton size='large' color='inherit' onClick={onNewRound}>
-              <Refresh />
-            </IconButton>
-            <IconButton size='large' color='inherit' onClick={onLeaveRoom}>
-              <ExitToApp />
-            </IconButton>
-            <IconButton
-              size='large'
-              color='inherit'
-              onClick={() => {
-                onSetUserName(undefined)
-                onLeaveRoom()
-              }}
-            >
-              <Person />
-            </IconButton>
-          </Fragment>
-        ) : undefined}
-      </Toolbar>
+        <Typography variant='h6' noWrap>
+          Header
+        </Typography>
     </AppBar>
   )
 }
